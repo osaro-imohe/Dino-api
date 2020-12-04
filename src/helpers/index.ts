d05d59afeff7cfea3fd43907da3863a0c1f589a3
Osaro <osaroimohe99@yahoo.com>
2020-12-04 20:33:31 +0100

    edit helpers

--- src/helpers/index.ts
+++ src/helpers/index.ts
@@ -2,8 +2,8 @@ export const generateInviteCode = () => {
   const length = 10;
   const chars =
     "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
-  var result = "";
-  for (var i = length; i > 0; --i)
+  let result = "";
+  for (let i = length; i > 0; --i)
     result += chars[Math.floor(Math.random() * chars.length)];
   return result;
 };
